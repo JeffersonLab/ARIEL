f28fbb13bbdd9835fa31a136123f6fd02de97cce
Ole Hansen <ole@jlab.org>
2018-05-07 01:18:22 -0400

    Un-UPSify FindUpsRoot and FindUpsBoost

--- buildtools/Modules/FindUpsBoost.cmake
+++ buildtools/Modules/FindUpsBoost.cmake
@@ -1,4 +1,4 @@
-# Boost is a very special case
+# Find Boost libraries
 #
 # find_ups_boost(  [minimum] )
 #  minimum - optional minimum version
@@ -9,13 +9,6 @@ include(CheckUpsVersion)
 
 # since variables are passed, this is implemented as a macro
 macro( find_ups_boost )
-
-# Check if the boost library has been set
-# boost is a special case
-SET ( BOOST_VERS $ENV{BOOST_VERSION} )
-IF (NOT BOOST_VERS)
-    MESSAGE (FATAL_ERROR "Boost library has not been setup")
-ENDIF()
 
 cmake_parse_arguments( FUB "" "" "" ${ARGN} )
 set( minimum )
@@ -50,59 +43,36 @@ if( ${found_product_match} LESS 0 )
     find_ups_boost( ${minimum} )")
   set(cet_product_list boost ${cet_product_list} )
 
-  # convert vx_y_z to x.y.z
-  STRING( REGEX REPLACE "v(.*)_(.*)_(.*)" "\\1.\\2.\\3" THISVER "${BOOST_VERS}" )
-  # find_package chokes on our trailing characters such as 1.57.0a, so these must be stripped
-  STRING( REGEX REPLACE "v(.*)_(.*)_([0-9]+).*" "\\1.\\2.\\3" MATCHVER "${BOOST_VERS}" )
-  #message(STATUS "find_ups_boost debug: have ${THISVER} and ${MATCHVER}" )
-  if( minimum )
-    STRING( REGEX REPLACE "v(.*)_(.*)_(.*)" "\\1.\\2" MINVER "${minimum}" )
-    #message(STATUS "find_ups_boost debug: Boost minimum version is ${MINVER} from ${minimum} " )
-    #upmessage(STATUS "find_ups_boost debug: Boost  version is ${THISVER} from ${BOOST_VERS} " )
-    if(  ${THISVER} STRGREATER ${MINVER} )
-      #message( STATUS "find_ups_boost debug: Boost ${THISVER} meets minimum required version ${MINVER}")
-    else()
-      message( FATAL_ERROR "Boost ${THISVER} is less than minimum required version ${MINVER}")
-    endif()
+  # Hint at library location - if Boost is available in our software environment,
+  # prefer it over the system libraries
+  set(BOOST_ROOT $ENV{JERM_ROOT})
+
+  # Bes ure to find the threaed-safe libraries
+  set(Boost_USE_MULTITHREADED ON)
+
+  # Disable loading of possible CMake configuration files - their behavior may not be standardized
+
+  set(Boost_NO_BOOST_CMAKE ON)
+  #set(Boost_ADDITIONAL_VERSIONS "1.48" "1.48.0" "1.49" "1.49.0")
+
+  # Translate vx_y_z to x.y.z - minimum acceptable library version (if any)
+  if( ${minimum} )
+    _get_dotver( ${minimum} )
   endif()
 
-  SET ( BOOST_STRING $ENV{SETUP_BOOST} )
-  STRING( REGEX MATCH "[-][q]" has_qual "${BOOST_STRING}" )
-  STRING( REGEX MATCH "[-][j]" has_j "${BOOST_STRING}" )
-  if( has_qual )
-    if( has_j )
-       STRING( REGEX REPLACE ".*([-][q]+ )(.*)[ *]([-][-j])" "\\2" BOOST_QUAL "${BOOST_STRING}" )
-    else( )
-       STRING( REGEX REPLACE ".*([-][q]+ )(.*)" "\\2" BOOST_QUAL "${BOOST_STRING}" )
-    endif( )
-    #message(STATUS "find_ups_boost debug: Boost qualifier is ${BOOST_QUAL}")
-    STRING( REGEX REPLACE ":" ";" BOOST_QUAL_LIST "${BOOST_QUAL}" )
-    #message(STATUS "find_ups_boost debug: Boost qualifiers list: ${BOOST_QUAL_LIST}")
-    list(REMOVE_ITEM BOOST_QUAL_LIST debug opt prof)
-    #message(STATUS "find_ups_boost debug: Boost qualifiers are ${BOOST_QUAL_LIST}")
-    STRING( REGEX REPLACE ";" ":" BOOST_BASE_QUAL "${BOOST_QUAL_LIST}" )
-    #message(STATUS "find_ups_boost debug: Boost base qualifier is ${BOOST_BASE_QUAL}")
-  else( )
-    message(STATUS "WARNING: Boost has no qualifier")
-  endif( )
-  _cet_debug_message("find_ups_boost: Boost version and qualifier are ${BOOST_VERS} ${BOOST_QUAL}" )
-  #message(STATUS "find_ups_boost debug: Boost base qualifier is ${BOOST_BASE_QUAL}" )
+  # Find the package (using CMake's FindBoost)
+  find_package( Boost ${dotver} COMPONENTS ${boost_liblist} REQUIRED )
 
-  include_directories ( SYSTEM $ENV{BOOST_INC} )
-
-  # define the boost environment so we don't get system libraries
-  set(BOOST_ROOT $ENV{BOOST_DIR} )
-  set(BOOST_INCLUDEDIR $ENV{BOOST_INC} )
-  set(BOOST_LIBRARYDIR $ENV{BOOST_LIB} )
-  set(Boost_USE_MULTITHREADED ON)
-  #set(Boost_ADDITIONAL_VERSIONS "1.48" "1.48.0" "1.49" "1.49.0")
-  set(Boost_NO_SYSTEM_PATHS ON)
-  # search for Boost ${MATCHVER} or better libraries
-  find_package( Boost ${MATCHVER} COMPONENTS ${boost_liblist} )
-
-  #message(STATUS "find_ups_boost debug: Boost include directory is ${Boost_INCLUDE_DIR}" )
-  #message(STATUS "find_ups_boost debug: Boost library directory is ${BOOST_LIBRARYDIR}" )
+  # Build a UPS-style vx_y_z version string - the actual found version of Boost
+  string(CONCAT BOOST_VERS "v" ${Boost_MAJOR_VERSION} "_" ${Boost_MINOR_VERSION} "_" ${Boost_SUBMINOR_VERSION})
+  
+  #message(STATUS "find_ups_boost debug: Boost version is ${BOOST_VERS}" )
+  #message(STATUS "find_ups_boost debug: Boost include directory is ${Boost_INCLUDE_DIRS}" )
+  #message(STATUS "find_ups_boost debug: Boost library directory is ${Boost_LIBRARY_DIRS}" )
   #message(STATUS "find_ups_boost debug: Boost_FILESYSTEM_LIBRARY is ${Boost_FILESYSTEM_LIBRARY}" )
+
+  include_directories ( SYSTEM ${Boost_INCLUDE_DIRS} )
+
 endif()
 
 endmacro( find_ups_boost )
--- buildtools/Modules/FindUpsRoot.cmake
+++ buildtools/Modules/FindUpsRoot.cmake
@@ -1,7 +1,7 @@
-# ROOT is a special case
+# Find ROOT in environment and define useful variables (library locations etc.)
 #
 # find_ups_root(  [minimum] )
-#  minimum - optional minimum version 
+#  minimum - optional minimum version
 
 include(CheckUpsVersion)
 include(CMakeParseArguments)
@@ -9,7 +9,7 @@ include(CMakeParseArguments)
 function(_set_root_lib_vars)
   file(GLOB root_libs
     LIST_DIRECTORIES false
-    ${ROOTSYS}/lib/lib*.so ${ROOTSYS}/lib/lib*.a)
+    ${ROOT_LIB_DIR}/lib*.so ${ROOT_LIB_DIR}/lib*.a)
   foreach (root_lib_path ${root_libs})
     if (NOT root_lib_path MATCHES "Dict\\.")
       get_filename_component(root_lib_stem ${root_lib_path} NAME_WE)
@@ -30,119 +30,129 @@ endfunction()
 # since variables are passed, this is implemented as a macro
 macro( find_ups_root )
 
-# require ROOTSYS
-set( ROOTSYS $ENV{ROOTSYS} )
-if ( NOT ROOTSYS )
-  message(FATAL_ERROR "root has not been setup")
-endif ()
+  # require either ROOTSYS or root-config
+  set( ROOTSYS $ENV{ROOTSYS} )
+  find_program(ROOT_CONFIG_EXEC root-config PATHS ${ROOTSYS}/bin DOC "ROOT configuration utility program")
+  if(ROOT_CONFIG_EXEC)
+    execute_process(
+      COMMAND ${ROOT_CONFIG_EXEC} --prefix
+      OUTPUT_VARIABLE ROOTSYS
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
+    #message( STATUS "ROOTSYS = ${ROOTSYS}" )
+  else()
+    message(FATAL_ERROR "Cannot find ROOT")
+  endif()
 
-cmake_parse_arguments( FUR "" "" "" ${ARGN} )
-set( minimum )
+  # only execute if this macro has not already been called
+  if( NOT ROOT_VERSION )
+    ##message( STATUS "find_ups_root debug: ROOT_VERSION is NOT defined" )
 
-# only execute if this macro has not already been called
-if( NOT ROOT_VERSION )
-  ##message( STATUS "find_ups_root debug: ROOT_VERSION is NOT defined" )
+    execute_process(COMMAND ${ROOT_CONFIG_EXEC} --version
+      OUTPUT_VARIABLE ROOT_CONFIG_VERSION
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
+    message( STATUS "Found ROOT version ${ROOT_CONFIG_VERSION}" )
+    string( REGEX REPLACE "^([0-9]+)\\.([0-9]+)/([0-9]+).*" "v\\1_\\2_\\3" ROOT_VERSION "${ROOT_CONFIG_VERSION}" )
 
-SET ( ROOT_STRING $ENV{SETUP_ROOT} )
-set( ROOT_VERSION $ENV{ROOT_VERSION} )
-if ( NOT ROOT_VERSION )
-   #message( STATUS "find_ups_root: calculating root version" )
-   STRING( REGEX REPLACE "^[r][o][o][t][ ]+([^ ]+).*" "\\1" ROOT_VERSION "${ROOT_STRING}" )
-endif ()
+    cmake_parse_arguments( FUR "" "" "" ${ARGN} )
+    set( minimum )
+    if( FUR_UNPARSED_ARGUMENTS )
+      list( GET FUR_UNPARSED_ARGUMENTS 0 minimum )
+      #message( STATUS "find_ups_root: checking root ${ROOT_VERSION} against ${minimum}" )
+      _check_version( ROOT ${ROOT_VERSION} ${minimum} )
+    endif()
 
-if( FUR_UNPARSED_ARGUMENTS )
-  list( GET FUR_UNPARSED_ARGUMENTS 0 minimum )
-  #message( STATUS "find_ups_root: checking root ${ROOT_VERSION} against ${minimum}" )
-  _check_version( ROOT ${ROOT_VERSION} ${minimum} )
-endif()
- _get_dotver( ${ROOT_VERSION} )
- set( ROOT_DOT_VERSION ${dotver} )
-# compare for recursion
-list(FIND cet_product_list root found_product_match)
-if( ${found_product_match} LESS 0 )
-  # add to product list
-  set(CONFIG_FIND_UPS_COMMANDS "${CONFIG_FIND_UPS_COMMANDS}
+    _get_dotver( ${ROOT_VERSION} )
+    set( ROOT_DOT_VERSION ${dotver} )
+    # compare for recursion
+    list(FIND cet_product_list root found_product_match)
+    if( ${found_product_match} LESS 0 )
+      # add to product list
+      set(CONFIG_FIND_UPS_COMMANDS "${CONFIG_FIND_UPS_COMMANDS}
     find_ups_root( ${minimum} )")
-  set(cet_product_list root ${cet_product_list} )
-endif()
+      set(cet_product_list root ${cet_product_list} )
+    endif()
 
-STRING( REGEX MATCH "[-][q]" has_qual  "${ROOT_STRING}" )
-STRING( REGEX MATCH "[-][j]" has_j  "${ROOT_STRING}" )
-if( has_qual )
-  if( has_j )
-     STRING( REGEX REPLACE ".*([-][q]+ )(.*)[ *]([-][-j])" "\\2" ROOT_QUAL "${ROOT_STRING}" )
-  else( )
-     STRING( REGEX REPLACE ".*([-][q]+ )(.*)" "\\2" ROOT_QUAL "${ROOT_STRING}" )
-  endif( )
-  STRING( REGEX REPLACE ":" ";" ROOT_QUAL_LIST "${ROOT_QUAL}" )
-  list(REMOVE_ITEM ROOT_QUAL_LIST debug opt prof)
-  STRING( REGEX REPLACE ";" ":" ROOT_BASE_QUAL "${ROOT_QUAL_LIST}" )
-else( )
-  message(STATUS "ROOT has no qualifier")
-endif( )
-if( ${found_product_match} LESS 0 )
-  _cet_debug_message("find_ups_root: ROOT version and qualifier are ${ROOT_VERSION} ${ROOT_QUAL}" )
-endif()
-#message(STATUS "ROOT base qualifier is ${ROOT_BASE_QUAL}" )
+    execute_process(
+      COMMAND ${ROOT_CONFIG_EXEC} --bindir
+      OUTPUT_VARIABLE ROOT_BIN_DIR
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if(NOT ROOT_BIN_DIR)
+      message( FATAL_ERROR "No ROOT bindir")
+    endif()
+    execute_process(
+      COMMAND ${ROOT_CONFIG_EXEC} --libdir
+      OUTPUT_VARIABLE ROOT_LIB_DIR
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if(NOT ROOT_LIB_DIR)
+      message( FATAL_ERROR "No ROOT libdir")
+    endif()
+    execute_process(
+      COMMAND ${ROOT_CONFIG_EXEC} --incdir
+      OUTPUT_VARIABLE ROOT_INC_DIR
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
+    if(NOT ROOT_INC_DIR)
+      message( FATAL_ERROR "No ROOT incdir")
+    endif()
 
-# add include directory to include path if it exists
-include_directories ( $ENV{ROOT_INC} )
+    # add include directory to include path if it exists
+    include_directories ( ROOT_INC_DIR )
 
-check_ups_version(root ${ROOT_VERSION} v6_00_00
-  PRODUCT_OLDER_VAR HAVE_ROOT5
-  PRODUCT_MATCHES_VAR HAVE_ROOT6
-  )
+    check_ups_version(root ${ROOT_VERSION} v6_00_00
+      PRODUCT_OLDER_VAR HAVE_ROOT5
+      PRODUCT_MATCHES_VAR HAVE_ROOT6
+      )
 
-check_ups_version(root ${ROOT_VERSION} v6_10_04
-  PRODUCT_MATCHES_VAR ROOT6_HAS_NOINCLUDEPATHS
-  )
+    check_ups_version(root ${ROOT_VERSION} v6_10_04
+      PRODUCT_MATCHES_VAR ROOT6_HAS_NOINCLUDEPATHS
+      )
 
-include_directories ( ${ROOTSYS}/include )
+    # Set library variables
+    _set_root_lib_vars()
 
-# Set library variables
-_set_root_lib_vars()
+    # define genreflex executable
+    _set_and_check_prog(ROOT_GENREFLEX ${ROOT_BIN_DIR}/genreflex)
 
-# define genreflex executable
-_set_and_check_prog(ROOT_GENREFLEX ${ROOTSYS}/bin/genreflex)
+    # check for the need to cleanup after genreflex
+    check_ups_version(root ${ROOT_VERSION} v5_28_00d PRODUCT_OLDER_VAR GENREFLEX_CLEANUP)
+    #message(STATUS "genreflex cleanup status: ${GENREFLEX_CLEANUP}")
 
-# check for the need to cleanup after genreflex
-check_ups_version(root ${ROOT_VERSION} v5_28_00d PRODUCT_OLDER_VAR GENREFLEX_CLEANUP)
-#message(STATUS "genreflex cleanup status: ${GENREFLEX_CLEANUP}")
+    # define rootcint executable
+    if (HAVE_ROOT6)
+      _set_and_check_prog(ROOTCLING ${ROOT_BIN_DIR}/rootcling)
+    else() # ROOT5
+      _set_and_check_prog(ROOTCINT ${ROOT_BIN_DIR}/rootcint)
+    endif()
 
-# define rootcint executable
-if (HAVE_ROOT6)
-  _set_and_check_prog(ROOTCLING ${ROOTSYS}/bin/rootcling)
-else() # ROOT5
-  _set_and_check_prog(ROOTCINT ${ROOTSYS}/bin/rootcint)
-endif()
-
-# define some useful library lists
-set(ROOT_BASIC_LIB_LIST ${ROOT_CORE}
-                        ${ROOT_CINT} 
-                        ${ROOT_RIO}
-                        ${ROOT_NET}
-                        ${ROOT_IMT}
-                        ${ROOT_HIST} 
-                        ${ROOT_GRAF}
-                        ${ROOT_GRAF3D}
-                        ${ROOT_GPAD}
-                        ${ROOT_TREE}
-                        ${ROOT_RINT}
-                        ${ROOT_POSTSCRIPT}
-                        ${ROOT_MATRIX}
-                        ${ROOT_PHYSICS}
-                        ${ROOT_MATHCORE}
-                        ${ROOT_THREAD}
-)
-set(ROOT_GUI_LIB_LIST   ${ROOT_GUI} ${ROOT_BASIC_LIB_LIST} )
-set(ROOT_EVE_LIB_LIST   ${ROOT_EVE}
-                        ${ROOT_EG}
-                        ${ROOT_TREEPLAYER}
-                        ${ROOT_GEOM}
-                        ${ROOT_GED}
-                        ${ROOT_RGL}
-                        ${ROOT_GUI_LIB_LIST}
-)
-endif( NOT ROOT_VERSION )
+    # define some useful library lists
+    set(ROOT_BASIC_LIB_LIST ${ROOT_CORE}
+      ${ROOT_CINT}
+      ${ROOT_RIO}
+      ${ROOT_NET}
+      ${ROOT_IMT}
+      ${ROOT_HIST}
+      ${ROOT_GRAF}
+      ${ROOT_GRAF3D}
+      ${ROOT_GPAD}
+      ${ROOT_TREE}
+      ${ROOT_RINT}
+      ${ROOT_POSTSCRIPT}
+      ${ROOT_MATRIX}
+      ${ROOT_PHYSICS}
+      ${ROOT_MATHCORE}
+      ${ROOT_THREAD}
+      )
+    set(ROOT_GUI_LIB_LIST   ${ROOT_GUI} ${ROOT_BASIC_LIB_LIST} )
+    set(ROOT_EVE_LIB_LIST   ${ROOT_EVE}
+      ${ROOT_EG}
+      ${ROOT_TREEPLAYER}
+      ${ROOT_GEOM}
+      ${ROOT_GED}
+      ${ROOT_RGL}
+      ${ROOT_GUI_LIB_LIST}
+      )
+  endif( NOT ROOT_VERSION )
 
 endmacro( find_ups_root )
