efaa45c1441befb3778bae0a7e60faa39027297f
Ole Hansen <ole@jlab.org>
2020-09-09 21:55:45 -0400

    art: Let memoryReportDarwin print dummy output
    To help make a handful of art integration tests in critic
    work cross-platform without the cumbersome keeping of
    platform-specific test reference files.
    
    One wonders how these tests (TimeTracker, BlockingPrescaler, etc.)
    ever succeeded on Linux since the MemReport section was missing from
    all the test output reference files since art commit f4a4114 dated
    25-May-2017. As if the art/critic integration tests were only
    exercised on macOS until recently?

--- src/art/art/Framework/EventProcessor/detail/memoryReportDarwin.cc
+++ src/art/art/Framework/EventProcessor/detail/memoryReportDarwin.cc
@@ -1,7 +1,17 @@
 #include "art/Framework/EventProcessor/detail/memoryReport.h"
+//#include "art/Utilities/DarwinProcMgr.h" // TODO
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+using mf::LogPrint;
 
 void
 art::detail::memoryReport()
 {
-  // Not implemented for Darwin
+  // Dummy implementation for Darwin
+  //DarwinProcMgr procInfo{}; // TODO
+  LogPrint("ArtSummary") << "MemReport  "
+                         << "---------- Memory summary [base-10 MB] ------";
+  LogPrint("ArtSummary") << "MemReport  VmPeak = " << 0.0
+                         << " VmHWM = " << 0.0;
+  LogPrint("ArtSummary") << "";
 }
