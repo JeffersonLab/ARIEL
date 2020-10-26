ab2f34ed6ef3939b76e34323816019905a00e6a1
Ole Hansen <ole@jlab.org>
2020-10-25 22:10:45 -0400

    Support setup via environment modules

--- cmake/Modules/ArielCreateSetup.cmake
+++ cmake/Modules/ArielCreateSetup.cmake
@@ -6,11 +6,11 @@ endif()
 
 configure_file(
   ${CMAKE_CURRENT_LIST_DIR}/../templates/setup.sh.in
-  ${CMAKE_CURRENT_BINARY_DIR}/setup.sh
+  ${CMAKE_CURRENT_BINARY_DIR}/setup-ariel.sh
   @ONLY
   )
 
 install(
-  FILES ${CMAKE_CURRENT_BINARY_DIR}/setup.sh
+  FILES ${CMAKE_CURRENT_BINARY_DIR}/setup-ariel.sh
   DESTINATION "${${product}_bin_dir}"
   )
